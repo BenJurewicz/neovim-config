bb64c4b7a033c83ad3e84cf65b0fd65d9b82322c
Benjamin Jurewicz <94708038+BenJurewicz@users.noreply.github.com>
2025-11-11 04:39:23 +0100

    Add additional keywords to TODOs searching

---
+++ lua/plugins/todo.lua
@@ -0,0 +1,22 @@
+return {
+    "folke/todo-comments.nvim",
+    lazy = true,
+    cmd = { "TodoTrouble" },
+    event = "LazyFile",
+    keys = {
+        {
+            "<leader>sT",
+            function()
+                Snacks.picker.todo_comments({
+                    keywords = { "TODO", "FIX", "FIXME", "BUG", "FIXIT", "ISSUE", "WARN", "WARNING" },
+                })
+            end,
+            desc = "Awoo Todo/Fix/Fixme",
+        },
+        {
+            "<leader>xT",
+            "<cmd>Trouble todo toggle filter = {tag = {TODO, FIX, FIXME, BUG, FIXIT, ISSUE, WARN, WARNING}}<cr>",
+            desc = "Todo/Fix/Fixme (Trouble)",
+        },
+    },
+}
