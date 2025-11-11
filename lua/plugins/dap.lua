60b7c39f2cc007ea0e30ab2b5e47d1f539ef075d
Benjamin Jurewicz <94708038+BenJurewicz@users.noreply.github.com>
2025-11-11 03:43:44 +0100

    Add extra keybindings for debugging

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -13,32 +13,35 @@ return {
         desc = "Step Over",
       },
     },
-    -- config = function()
-    --   local dap = require("dap")
-    --   local wk = require("which-key")
-    --
-    --   local debug_keybinds = {
-    --     { "<Left>", dap.step_out, desc = "Step Out" },
-    --     { "<Right>", dap.step_into, desc = "Step Into" },
-    --     { "<Down>", dap.step_over, desc = "Step Over" },
-    --     { "<Up>", dap.continue, desc = "Continue" },
-    --   }
-    --
-    --   local cleanup_keymaps = function()
-    --     local keys = { "<Left>", "<Right>", "<Down>", "<Up>" }
-    --     for _, key in ipairs(keys) do
-    --       pcall(function()
-    --         vim.keymap.del("n", key)
-    --       end)
-    --     end
-    --   end
-    --
-    --   dap.listeners.after.event_initialized["dap_keymaps"] = function()
-    --     wk.add(debug_keybinds)
-    --   end
-    --
-    --   dap.listeners.before.event_terminated["dap_keymaps"] = cleanup_keymaps
-    --   dap.listeners.before.event_exited["dap_keymaps"] = cleanup_keymaps
-    -- end,
+    opts = function(_, opts)
+      local dap = require("dap")
+
+      local debug_keybinds = {
+        { "<Left>", dap.step_out, desc = "Step Out" },
+        { "<Right>", dap.step_into, desc = "Step Into" },
+        { "<Down>", dap.step_over, desc = "Step Over" },
+        { "<Up>", dap.continue, desc = "Continue" },
+        { "q", dap.terminate, desct = "terminate" },
+      }
+
+      local cleanup_keymaps = function()
+        for _, binds in ipairs(debug_keybinds) do
+          pcall(function()
+            vim.keymap.del("n", binds[1])
+          end)
+        end
+      end
+
+      dap.listeners.after.event_initialized["dap_keymaps"] = function()
+        for _, bind in ipairs(debug_keybinds) do
+          vim.keymap.set("n", bind[1], bind[2], { desc = bind[3] })
+        end
+      end
+
+      dap.listeners.before.event_terminated["dap_keymaps"] = cleanup_keymaps
+      dap.listeners.before.event_exited["dap_keymaps"] = cleanup_keymaps
+
+      return opts
+    end,
   },
 }
