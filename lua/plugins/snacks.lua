927d2f837c7f56813d37303daaa53daf3f906d48
Benjamin Jurewicz <94708038+BenJurewicz@users.noreply.github.com>
2026-06-28 00:18:34 +0200

    ui: Make scratch buffers fullscreen

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,6 +1,13 @@
 return {
     "folke/snacks.nvim",
     opts = {
+        scratch = {
+            win = {
+                width = 0,
+                height = 0,
+                border = false,
+            },
+        },
         zen = {
             toggles = {
                 dim = false,
